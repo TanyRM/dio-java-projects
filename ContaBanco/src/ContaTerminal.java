40b3332bc72ca56ff3fdf9615eb1bb0f2c14acf6
TanyRM <tanielemadu@outlook.com>
2023-08-29 10:21:02 -0300

    implementação da classe ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\nPor favor, digite o nome do cliente: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.println("\nPor favor, digite sua agência: ");
+        String agencia = scan.nextLine();
+
+        System.out.println("\nPor favor, digite o número da conta: ");
+        int numero = scan.nextInt();
+
+        System.out.println("\nInsira o valor do seu saldo: "); //Separado por viírgula
+        double saldo = scan.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        
+        scan.close();
     }
 }
